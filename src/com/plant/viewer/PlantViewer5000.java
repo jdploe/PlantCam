c61b560253378458ce12fbee81330cbe7884dfef
jdploe <jdploe@mtu.edu>
2017-05-07 12:46:33 -0500

    converted to maven project

---
+++ src/com/plant/viewer/PlantViewer5000.java
@@ -0,0 +1,11 @@
+package com.plant.viewer;
+
+public class PlantViewer5000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is PlantViewer 5000!");
+	}
+
+}
