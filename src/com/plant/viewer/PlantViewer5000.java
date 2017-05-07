1dae933a31a5c609db0cf86ccba0e16ef6b51147
jdploe <jdploe@mtu.edu>
2017-05-07 14:53:24 -0500

    initial commit

--- src/com/plant/viewer/PlantViewer5000.java
+++ src/com/plant/viewer/PlantViewer5000.java
@@ -1,11 +1,37 @@
 package com.plant.viewer;
 
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.video.ConverterFactory;
+import com.xuggle.xuggler.video.IConverter;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IVideoPicture;
+
+
+
 public class PlantViewer5000 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println("This is PlantViewer 5000!");
+
+		
+
+		System.out.println("DONE!");
 	}
+
 
 }
